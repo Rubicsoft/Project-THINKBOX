40b826382111f41d6e319234982820e6c773f4ca
Reymo Je <rubicsoft0@gmail.com>
2024-11-14 23:35:46 +0700

    Add UCOK(Suzanne)

--- scripts/ui/hud.gd
+++ scripts/ui/hud.gd
@@ -1,15 +1,19 @@
 extends Control
 
 @export var interact_raycast: RayCast3D
-@onready var label = $Label
+@onready var collide_label = $CollideWith
+@onready var frame_rate = $FrameRate
+
 
 func _ready():
 	pass # Replace with function body.
 
 func _process(delta):
-	label.text = ""
+	collide_label.text = ""
 	if interact_raycast.is_colliding():
 		var collider = interact_raycast.get_collider()
 		#label.text = collider.name
 		if collider is Interactable:
-			label.text = collider.prompt_msg
+			collide_label.text = collider.prompt_msg
+	
+	frame_rate.text = "FPS " + str(Engine.get_frames_per_second())
