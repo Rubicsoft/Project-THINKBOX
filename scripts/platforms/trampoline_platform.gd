80e7c625ad8ba0b61d6124a6e33ab8d85c9e5b99
Rubycsoft <rubicsoft0@gmail.com>
2024-11-29 10:03:00 +0700

    Placing the trampolines

--- scripts/platforms/trampoline_platform.gd
+++ scripts/platforms/trampoline_platform.gd
@@ -1,9 +1,10 @@
 extends Area3D
 
+@export var JUMP_FORCE: float = 10.0
+
 func make_jump(player: CharacterBody3D) -> void:
-	player.velocity.y = 10.0
+	player.velocity.y = JUMP_FORCE
 
 func _on_body_entered(body: Node3D) -> void:
-	print("Player collide")
 	if body is CharacterBody3D:
 		make_jump(body)
