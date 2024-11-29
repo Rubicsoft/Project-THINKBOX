fd66d66a823a18ed3344c867a8236bf600d5f6f3
Reymo Je <rubicsoft0@gmail.com>
2024-11-29 21:21:53 +0700

    Update trampoline

--- scripts/platforms/trampoline_platform.gd
+++ scripts/platforms/trampoline_platform.gd
@@ -3,7 +3,8 @@ extends Area3D
 @export var JUMP_FORCE: float = 10.0
 
 func make_jump(player: CharacterBody3D) -> void:
-	player.velocity.y = JUMP_FORCE
+	if player.velocity.y:
+		player.velocity.y = JUMP_FORCE
 
 func _on_body_entered(body: Node3D) -> void:
 	if body is CharacterBody3D:
