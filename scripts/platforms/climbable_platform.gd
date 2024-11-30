b5edb59ab466253d2dc1551c988d3638b0e3c215
Reymo Je <rubicsoft0@gmail.com>
2024-11-30 10:43:48 +0700

    Update climbable platform

---
+++ scripts/platforms/climbable_platform.gd
@@ -0,0 +1,21 @@
+extends Area3D
+
+var player: CharacterBody3D
+var is_climbing: bool = false
+
+const CLIMB_SPEED = 4.5
+const CLIMB_SMOOTHNESS = 12.5
+
+func _physics_process(delta):
+	if is_climbing and Input.is_action_pressed("move_foreward"):
+		print("Climb")
+		player.velocity.y = CLIMB_SPEED
+
+func _on_body_entered(body) -> void:
+	if body is CharacterBody3D:
+		print("Climb")
+		player = body
+		is_climbing = true
+
+func _on_body_exited(body):
+	is_climbing = false
