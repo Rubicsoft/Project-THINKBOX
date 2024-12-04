6a6c1929b61a53725f921043c20fb0b45210b3f8
Reymo Je <rubicsoft0@gmail.com>
2024-12-04 12:02:00 +0700

    Add opening cutscene code

--- scripts/game_state.gd
+++ scripts/game_state.gd
@@ -1,9 +1,20 @@
 extends Node3D
 
+@export var opening_cutscene: AnimationPlayer
+
 func _ready() -> void:
+	if opening_cutscene:
+		opening_cutscene.connect("animation_finished", Callable(self, "_on_opening_cutscene_finished"))
+	# Make the mouse invisible and centered when in gameplay
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	if opening_cutscene:
+		opening_cutscene.play()
+		Global.is_player_controllable = false
 
 func _process(delta) -> void:
 	print("Player life left " + str(Global.get_value("life_left")))
 	if Global.get_value("life_left") <= 0:
 		get_tree().quit()
+
+func _on_opening_cutscene_finished(anim_name: StringName) -> void:
+	Global.is_player_controllable = true
