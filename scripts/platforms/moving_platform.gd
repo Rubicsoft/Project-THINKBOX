8aed5ce09136d911893b6cbd7c3105a3ef5cdea0
Reymo Je <rubicsoft0@gmail.com>
2024-12-21 10:15:48 +0700

    Working on moving platform

--- scripts/platforms/moving_platform.gd
+++ scripts/platforms/moving_platform.gd
@@ -5,3 +5,10 @@ extends AnimatableBody3D
 @export_range(0.0, 10.0, 0.1) var duration: float = 5.0
 
 var start_position: Vector3
+
+func _ready():
+	start_position = global_position
+	
+	if end_position and is_moving:
+		var moving = get_tree().create_tween()
+		moving.tween_property(self, "global_position", end_position.global_position, duration)
