7f31fce26d04230ee3ee288c1c77ea58e126f191
Reymo Je <rubicsoft0@gmail.com>
2024-12-21 20:53:19 +0700

    Add animation to Codex

--- scripts/collectibles/codex.gd
+++ scripts/collectibles/codex.gd
@@ -1,4 +1,9 @@
 extends Area3D
 
+@onready var animation = $AnimationPlayer
+@onready var pickup_sfx = $PickUp
+
 func _on_body_entered(body):
 	print("CODEX")
+	animation.play("codex_pickup")
+	pickup_sfx.play()
