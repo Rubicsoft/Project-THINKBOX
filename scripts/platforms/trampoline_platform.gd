8416eaedc237df42a0662ad3c8811fab7988bcc6
Reymo Je <rubicsoft0@gmail.com>
2024-12-30 23:14:28 +0700

    Editing levels + recallibrate trampoline platforms

--- scripts/platforms/trampoline_platform.gd
+++ scripts/platforms/trampoline_platform.gd
@@ -2,11 +2,11 @@ extends Area3D
 
 @onready var boing_sfx = $BoingSFX
 
-@export_range(1.0, 10.0, 0.1) var JUMP_FORCE: float = 10.0
+@export_range(1.0, 20.0, 0.1) var JUMP_FORCE: float = 10.0
 
 func make_jump(player: CharacterBody3D) -> void:
 	if player.velocity.y:
-		player.velocity.y = JUMP_FORCE * 2.0
+		player.velocity.y = JUMP_FORCE * 1.5
 		player.jump(false) # Play jump effect
 		boing_sfx.play()
 
