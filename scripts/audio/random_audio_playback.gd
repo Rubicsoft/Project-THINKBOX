9370a4a76f813c1ebe5422bc7bb272a92aeabaef
Reymo Je <rubicsoft0@gmail.com>
2025-02-10 20:41:19 +0700

    Fix the random audio playback array problem Before that it is only limited to 3 AudioStreams, but now it is unlimited

--- scripts/audio/random_audio_playback.gd
+++ scripts/audio/random_audio_playback.gd
@@ -1,28 +1,31 @@
 extends AudioStreamPlayer3D
 class_name RandomAudioPlayback
 
+
 @export var is_playable: bool = true
 @export var continuity: bool = false
+@export var audio_library: Array[AudioStream]
 
-# NOTE : Issue! if the audios imported is less than 3, the audio will stop playing and not looping
-
-@export var audio_1: AudioStream
-@export var audio_2: AudioStream
-@export var audio_3: AudioStream
-
-var sounds = []
 
 func _ready() -> void:
 	connect("finished", Callable(self, "_on_finished"))
-	sounds = [audio_1, audio_2, audio_3]
 
+
+# Play the random audio
 func play_audio() -> void:
 	if is_playable:
 		if not continuity:
 			is_playable = false
-		var random_sfx = sounds[randi() % sounds.size()]
-		stream = random_sfx
-		play()
+		
+		if not audio_library.is_empty():
+			var random_sfx = audio_library[randi() % audio_library.size()]
+			stream = random_sfx
+			play()
+		else:
+			printerr("No random audio playback inputed")
+
+
+# ------ SIGNAL ------
 
 func _on_finished() -> void:
 	is_playable = true
