6f3a8dc4861b0d9fc235a53237e40cc86a5ddc0f
Reymo Je <rubicsoft0@gmail.com>
2025-02-10 20:56:51 +0700

    Recode the MovingPlatform to tidy up and fix its script Remove the delay checkbox Add new sine interpolation checkbox

--- scripts/platforms/moving_platform.gd
+++ scripts/platforms/moving_platform.gd
@@ -1,11 +1,13 @@
 extends AnimatableBody3D
 class_name FloatingPlatform
 
+
 @export var is_moving: bool = false
 @export var end_position: Node3D
 @export_range(0.0, 10.0, 0.1) var duration: float = 5.0
-@export var delay: bool = false
-@export_range(0.0, 10.0, 0.1) var delay_time: float = 1.0
+@export_range(0.0, 10.0, 0.1) var delay_time: float = 0.0
+@export var sine_interpolation: bool = false
+
 
 var start_position: Vector3
 
@@ -15,27 +17,25 @@ func _ready():
 	tweening()
 	
 
+# Move the platform using Tweening method
 func tweening() -> void:
 	if end_position and is_moving:
+		# Tweening setup
 		var tween = get_tree().create_tween()
 		tween.set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
 		tween.set_loops()
-		tween.set_ease(Tween.EASE_IN_OUT)
-		tween.set_trans(Tween.TRANS_SINE)
+		if sine_interpolation:
+			tween.set_ease(Tween.EASE_IN_OUT)
+			tween.set_trans(Tween.TRANS_SINE)
 		
-		delay_between(tween)
+		# Tweening movement
+		tween.tween_interval(delay_time)
 		tween.tween_property(self, "global_position", end_position.global_position, calculate_duration())
-		delay_between(tween)
+		
+		tween.tween_interval(delay_time)
 		tween.tween_property(self, "global_position", global_position, calculate_duration())
 
 
+# Calculate the duration of the moving platform
 func calculate_duration() -> float:
-	if delay:
-		return duration/2.0 - delay_time
-	else:
-		return duration/2.0
-
-
-func delay_between(tween) -> void:
-	if delay:
-		tween.tween_interval(delay_time)
+	return duration/2.0 - delay_time
