f59710de741681db48f18c063d9685f2fb4728db
Reymo Je <rubicsoft0@gmail.com>
2025-02-28 16:46:45 +0700

    Game System : Enhance the Dialogue Handler instruction

--- scripts/dialogue/dialogue_trigger.gd
+++ scripts/dialogue/dialogue_trigger.gd
@@ -3,20 +3,11 @@ extends Area3D
 @export var dialog_handler: DialogHandler
 
 
-var dialogue_dict: Dictionary = {}
-
-
 # SIGNAL
 
 func _on_trigger_entered(body: Node3D) -> void:
 	if dialog_handler:
-		dialogue_dict = dialog_handler.dialogue_dict
-		dialog_handler.set_gui_dialogue()
+		dialog_handler.play = true
 	else:
-		printerr("DialogHandler Node not found")
-	
-	#if dialogue_dict != {}:
-		#for i in range(dialogue_dict.get("chats", {}).size()):
-			#if Global.get_global_condition("is_on_dialogue"):
-				#print(dialogue_dict["chats"][i]["dialogue"])
+		printerr("DialogHandler Node not found in " + name)
 	
