8975f78e84ab32e64e24ec6668df5e25966b9b24
Reymo Je <rubicsoft0@gmail.com>
2025-03-20 22:10:32 +0700

    Game System : Added QuickClimb Zone to enhance quickclimb mechanic Player now can be able to quick climb to any platform.
    NOTE : climbable_platform.gd code retidy

--- scripts/platforms/climbable_platform.gd
+++ scripts/platforms/climbable_platform.gd
@@ -1,19 +1,26 @@
 extends Area3D
 
+
 var player: CharacterBody3D
 var is_climbing: bool = false
 
 const CLIMB_SPEED = 4.5
 const CLIMB_SMOOTHNESS = 12.5
 
+
+
 func _physics_process(delta):
 	if is_climbing and Input.is_action_pressed("move_foreward"):
 		player.velocity.y = CLIMB_SPEED
+
+
+# ------ SIGNAL ------
 
 func _on_body_entered(body) -> void:
 	if body is Player:
 		player = body
 		is_climbing = true
+
 
 func _on_body_exited(body):
 	is_climbing = false
--- scripts/player/player_controller.gd
+++ scripts/player/player_controller.gd
@@ -148,10 +148,11 @@ func take_fall() -> void:
 # Quick climbing mechanic
 func quick_climbing() -> void:
 	if quickclimb_raycast.is_colliding() and not is_on_floor() and Input.is_action_pressed("move_foreward"):
-		var hit_obj: Object = quickclimb_raycast.get_collider()
-		if (hit_obj is FloatingPlatformFlat) or (hit_obj is FloatingPlatformSpin):
-			velocity.y = QUICKCLIMB_ENERGY
-			jump(false)
+		# Do Actions
+		print(quickclimb_raycast.get_collider())
+		
+		velocity.y = QUICKCLIMB_ENERGY
+		#jump(false)
 
 
 # Handle Jump
