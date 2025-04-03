268010ed174e09ccef424c847e9d483685941b37
Reymo Je <rubicsoft0@gmail.com>
2025-04-03 22:13:27 +0700

    Game System : Improve pause menu ui with Theme ON PROGRESS : For transition, I use tweener, but there is some error when creating the tweener.

--- scripts/ui/pause_menu.gd
+++ scripts/ui/pause_menu.gd
@@ -1,22 +1,29 @@
 extends Control
 
+
 @export var camera_fx: Control
-
-@onready var anim: AnimationPlayer = $AnimationPlayer
-
 
 var pausemenu_visibility: bool = false
 var bgm_normal_volume: float
+var menu_tween: Tween
 
 const BGM_PAUSED_VOLUME = -10.0
 const BGM_PAUSE_SMOOTHNESS = 5.0
 
+
+
 func _ready() -> void:
 	bgm_normal_volume = AudioServer.get_bus_volume_db(AudioServer.get_bus_index("BGM"))
+	
+	# ON PROGRESS : There are some error to the Tweener
+	menu_tween = create_tween()
+	#menu_tween.bind_node(self)
+
 
 func _input(event) -> void:
 	if event.is_action_pressed("esc") and not get_tree().paused and Global.get_global_condition("is_pausable"):
 		pause_game()
+
 
 func _process(_delta) -> void:
 	visible = pausemenu_visibility
@@ -29,23 +36,40 @@ func _process(_delta) -> void:
 			AudioServer.set_bus_volume_db(AudioServer.get_bus_index("BGM"), bgm_normal_volume)
 
 
+
+# ------ SUPPORTING FUNCTIONS ------
+
+# Pause the game
 func pause_game() -> void:
 	get_tree().paused = true
 	pausemenu_visibility = true
-	anim.play("pause_popup")
+	
+	# ON PROGRESS : Error to the Tweener
+	#modulate = Color.TRANSPARENT
+	#menu_tween.tween_property(self, "modulate", Color.WHITE, 0.5)
+	#get_tree().create_tween().tween_property(self, "modulate", Color.WHITE, 0.5)
+	modulate = Color.WHITE
 
+
+# Resume the game
 func resume_game() -> void:
 	get_tree().paused = false
+	#visible = false
+	modulate = Color.TRANSPARENT
+	pausemenu_visibility = false
 
+
+
+# ------ SIGNALS ------
 
 func _on_quit_btn_pressed() -> void:
 	get_tree().quit()
 
 
 func _on_resume_btn_pressed() -> void:
-	anim.play_backwards("pause_popup")
-	pausemenu_visibility = false
-	get_tree().paused = false
+	#pausemenu_visibility = false
+	#get_tree().paused = false
+	resume_game()
 
 
 func _on_restart_btn_pressed() -> void:
@@ -55,7 +79,6 @@ func _on_restart_btn_pressed() -> void:
 
 
 func _on_restart_checkpoint_btn_pressed() -> void:
-	anim.play_backwards("pause_popup")
 	pausemenu_visibility = false
 	
 	var parent: Node = get_parent()
