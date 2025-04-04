fb94649bbcb4d0c83d20c6eaf9ecfd7fb70e2d53
Reymo Je <rubicsoft0@gmail.com>
2025-04-04 20:01:13 +0700

    Game System : Add fade in-out effect to PauseMenu Also redesign the PauseMenu UI

--- scripts/ui/pause_menu.gd
+++ scripts/ui/pause_menu.gd
@@ -9,20 +9,22 @@ var menu_tween: Tween
 
 const BGM_PAUSED_VOLUME = -10.0
 const BGM_PAUSE_SMOOTHNESS = 5.0
+const fade_duration: float = 0.1
 
 
 
 func _ready() -> void:
 	bgm_normal_volume = AudioServer.get_bus_volume_db(AudioServer.get_bus_index("BGM"))
-	
-	# ON PROGRESS : There are some error to the Tweener
-	menu_tween = create_tween()
-	#menu_tween.bind_node(self)
 
 
 func _input(event) -> void:
-	if event.is_action_pressed("esc") and not get_tree().paused and Global.get_global_condition("is_pausable"):
-		pause_game()
+	# When PAUSE button is pressed, toggle the PauseMenu
+	if event.is_action_pressed("esc") and Global.get_global_condition("is_pausable"):
+		match get_tree().paused:
+			true:
+				resume_game()
+			false:
+				pause_game()
 
 
 func _process(_delta) -> void:
@@ -44,42 +46,59 @@ func pause_game() -> void:
 	get_tree().paused = true
 	pausemenu_visibility = true
 	
-	# ON PROGRESS : Error to the Tweener
-	#modulate = Color.TRANSPARENT
-	#menu_tween.tween_property(self, "modulate", Color.WHITE, 0.5)
-	#get_tree().create_tween().tween_property(self, "modulate", Color.WHITE, 0.5)
-	modulate = Color.WHITE
+	gui_fade(true, menu_tween, self, fade_duration)
 
 
 # Resume the game
 func resume_game() -> void:
 	get_tree().paused = false
-	#visible = false
-	modulate = Color.TRANSPARENT
+	
+	await gui_fade(false, menu_tween, self, fade_duration).finished
+	
 	pausemenu_visibility = false
+
+
+# Fade in/out to the GUI
+func gui_fade(fade_in: bool, tween: Tween, gui_node: Control, duration: float) -> Tween:
+	tween = get_tree().create_tween()
+	tween.set_ignore_time_scale()
+	tween.set_pause_mode(Tween.TWEEN_PAUSE_PROCESS)
+	
+	match fade_in:
+		true:
+			gui_node.modulate = Color.TRANSPARENT
+			tween.tween_property(gui_node, "modulate", Color.WHITE, duration)
+		
+		false:
+			gui_node.modulate = Color.WHITE
+			tween.tween_property(gui_node, "modulate", Color.TRANSPARENT, duration)
+	
+	return tween
 
 
 
 # ------ SIGNALS ------
 
+# Quit to Desktop
 func _on_quit_btn_pressed() -> void:
 	get_tree().quit()
 
 
+# Resume game
 func _on_resume_btn_pressed() -> void:
-	#pausemenu_visibility = false
-	#get_tree().paused = false
 	resume_game()
 
 
+# Restart level
 func _on_restart_btn_pressed() -> void:
 	get_tree().paused = false
 	Global.reset_global()
 	get_tree().reload_current_scene()
 
 
+# Restart from Checkpoint
 func _on_restart_checkpoint_btn_pressed() -> void:
-	pausemenu_visibility = false
+	resume_game()
 	
 	var parent: Node = get_parent()
 	if parent is Player:
@@ -92,5 +111,6 @@ func _on_restart_checkpoint_btn_pressed() -> void:
 		printerr("No Player node inserted")
 
 
+# Back to Main Menu
 func _on_main_menu_btn_pressed() -> void:
-	get_tree().change_scene_to_file("res://ui/main_menu.tscn")
+	get_tree().change_scene_to_file("res://scenes/main_menu_scene.tscn")
